93653ef2ef54b2634aee0b4bbb26687cc56c0832
dom <dom@marshall34.co.uk>
2026-02-01 16:52:09 +0000

    Change colour scheme to gruvbox

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,7 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "morhetz/gruvbox",
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("catppuccin-macchiato")
+    vim.cmd.colorscheme("gruvbox")
   end,
 }
